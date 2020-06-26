3bdcf1a9cd50833b632f6253f66fc512e60fbd3f
EnriqueJose99 <enrique.galeano99@yahoo.com>
2020-06-26 01:31:04 -0600

    Add files via upload

--- js/my-app.js
+++ js/my-app.js
@@ -29,16 +29,19 @@ var app = new Framework7({
     	url: 'profile.html',
     	name: 'profile',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/clases/',
     	url: 'clases.html',
     	name: 'clases',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/cursos/',
     	url: 'cursos.html',
     	name: 'cursos',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/info-g/',
     	url: 'info-g.html',
@@ -49,36 +52,43 @@ var app = new Framework7({
     	url: 'notas.html',
     	name: 'notas',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/contenido/',
     	url: 'contenido.html',
     	name: 'contenido',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/contenido-1/',
     	url: 'contenido-1.html',
     	name: 'contenido-1',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/recursos/',
     	url: 'recursos.html',
     	name: 'recursos',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/actividades/',
     	url: 'actividades.html',
     	name: 'actividades',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/bibliografia/',
     	url: 'bibliografia.html',
     	name: 'bibliografia',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/foro/',
     	url: 'foro.html',
     	name: 'foro',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/foro-1/',
     	url: 'foro-1.html',
@@ -98,9 +108,72 @@ var app = new Framework7({
 		path: '/settings/',
     	url: 'settings.html',
     	name: 'settings',
-  		}
-		
-		
+  		},
+		{
+		path: '/edicionvideo/',
+    	url: 'edicionvideo.html',
+    	name: 'edicionvideo',
+  		},
+		{
+		path: '/guion/',
+    	url: 'guion.html',
+    	name: 'guion',
+  		},
+		{
+		path: '/fotografia/',
+    	url: 'fotografia.html',
+    	name: 'fotografia',
+  		},
+		{
+		path: '/digital/',
+    	url: 'digital.html',
+    	name: 'digital',
+  		},
+		{
+		path: '/help/',
+    	url: 'help.html',
+    	name: 'help',
+  		},
+		{
+		path: '/configuracion/',
+    	url: 'configuracion.html',
+    	name: 'configuracion',
+  		},
+		{
+		path: '/editarcontraseña/',
+    	url: 'editarcontraseña.html',
+    	name: 'editarcontraseña',
+  		},
+		{
+		path: '/editarfoto/',
+    	url: 'editarfoto.html',
+    	name: 'editarfoto',
+  		},
+		{
+		path: '/numerostelefono/',
+    	url: 'numerostelefono.html',
+    	name: 'numerostelefono',
+  		},
+		{
+		path: '/info-personal/',
+    	url: 'info-personal.html',
+    	name: 'info-personal',
+  		},
+		{
+		path: '/correosunitec/',
+    	url: 'correosunitec.html',
+    	name: 'correosunitec',
+  		},
+		{
+		path: '/ubicaciones/',
+    	url: 'ubicaciones.html',
+    	name: 'ubicaciones',
+  		},
+		{
+		path: '/privacidad/',
+    	url: 'privacidad.html',
+    	name: 'privacidad',
+  		}		
 	]
 });
 // Export selectors engine
